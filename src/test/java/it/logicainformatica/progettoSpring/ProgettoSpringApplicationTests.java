b8f7b9f62ba3baf919de65bd9553b27dd5ea1097
Emanuele117 <emanuele.pica7@gmail.com>
2023-02-20 19:03:43 +0100

    first commit

---
+++ src/test/java/it/logicainformatica/progettoSpring/ProgettoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package it.logicainformatica.progettoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
