18a02194042b451c79c0975c54bc0f31015ae857
Emanuele117 <emanuele.pica7@gmail.com>
2023-02-22 13:14:15 +0100

    prova1 inserimento utente e scrittura file

--- src/main/java/it/logicainformatica/progettoSpring/database/AnagraficaDB.java
+++ src/main/java/it/logicainformatica/progettoSpring/database/AnagraficaDB.java
@@ -1,5 +1,7 @@
 package it.logicainformatica.progettoSpring.database;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -13,20 +15,25 @@ import it.logicainformatica.progettoSpring.bean.AnagraficaUtente;
 
 public class AnagraficaDB {
 
-	
+	//INSTANZA OGGETTO DATABASE
 	Database db = new Database();
 	
 	// INSERISCO UN NUOVO UTENTE
 	public void inserisciUtente(AnagraficaUtente a) {
 
+		// CREO L'OGGETTO CONNESSIONE
 		Connection dbconn = null;
+		
 		try {
 
+			// CONNESSIONE AL DATABASE
 			dbconn = db.getConnessione();
 
+			// QUERY OER INSERIRE I CAMPI NELLA TABELLA ANAGRAFICA
 			PreparedStatement statement = dbconn.prepareStatement(
 					"Insert into anagrafica(nome,cognome,email,telefono,CF,indirizzo) values (?,?,?,?,?,?)");
 
+			//SETTO ALLA QUERY LE VARIABILI DI ANAGRAFICA
 			statement.setString(1, a.getNome());
 			statement.setString(2, a.getCognome());
 			statement.setString(3, a.getEmail());
@@ -34,6 +41,7 @@ public class AnagraficaDB {
 			statement.setString(5, a.getCodiceFiscale());
 			statement.setString(6, a.getIndirizzo());
 
+			// VERIFICO L'INSERIMENTO DELLA QUERY
 			boolean f = statement.execute();
 
 			if (f == false) {
@@ -61,21 +69,20 @@ public class AnagraficaDB {
 	// STAMPO L'ANAGRAFICA INTERA
 	public List<AnagraficaUtente> getAnagraficaAll() {
 
-		// CREO L'OGGETTO CONNECTION
+		// CREO L'OGGETTO CONNESSIONE
 		Connection dbconn = null;
 
 		List<AnagraficaUtente> lista = new ArrayList<AnagraficaUtente>();
 
 		try {
 
-			// VALORIZZO L'OGGETTO CONNECTION
+			// VALORIZZO L'OGGETTO CONNESSIONE
 			dbconn = db.getConnessione();
 
-			// PREPARO L' ISTRUZIONE SQL
+			// PREPARO LA QUERY
 			PreparedStatement statement = dbconn.prepareStatement("SELECT * FROM anagrafica");
 
-			// LANCIO LA QUERY SUL DATA BASE E I RISULTATI ME LI RESTITUSCIE IN UN OGGETTO
-			// DI TIPO RESULTSET
+			// LANCIO LA QUERY SUL DATABASE E I RISULTATI ME LI RESTITUSCIE IN UN OGGETTO DI TIPO RESULTSET
 			ResultSet rs = statement.executeQuery();
 
 			// CICLO I VALORI CHE ARRIVANO DAL DB
@@ -83,8 +90,7 @@ public class AnagraficaDB {
 
 				AnagraficaUtente ana = new AnagraficaUtente();
 
-				// INSERENDO IL NOME DELLA COLONNA E MI PRENDO IL VALORE
-
+				// INSERISCO IL NOME DELLA COLONNA E MI PRENDO IL VALORE
 				ana.setNome(rs.getString("nome"));
 				ana.setCognome(rs.getString("cognome"));
 				ana.setId(rs.getInt("id"));
@@ -108,6 +114,64 @@ public class AnagraficaDB {
 		}
 
 		return lista;
+
+	}
+	
+	public void scrivoSuFile(AnagraficaUtente a) throws IOException {
+		
+		// CREO L'OGGETTO CONNESSIONE
+		Connection dbconn = null;
+		
+		// CREO FILE DI TESTO
+		FileWriter writer = new FileWriter("prova.txt");
+		
+		try {
+
+			// VALORIZZO L'OGGETTO CONNESSIONE
+			dbconn = db.getConnessione();
+
+			// PREPARO LA QUERY
+			PreparedStatement statement = dbconn.prepareStatement("SELECT * FROM anagrafica");
+
+			// LANCIO LA QUERY SUL DATABASE E I RISULTATI ME LI RESTITUSCIE IN UN OGGETTO DI TIPO RESULTSET
+			ResultSet rs = statement.executeQuery();
+
+			// CICLO I VALORI CHE ARRIVANO DAL DB
+			while (rs.next()) {
+
+				AnagraficaUtente ana = new AnagraficaUtente();
+
+				// INSERISCO IL NOME DELLA COLONNA E MI PRENDO IL VALORE
+				ana.setNome(rs.getString("nome"));
+				ana.setCognome(rs.getString("cognome"));
+				ana.setId(rs.getInt("id"));
+				ana.setTelefono(rs.getString("telefono"));
+				ana.setIndirizzo(rs.getString("indirizzo"));
+				ana.setCodiceFiscale(rs.getString("cf"));
+				ana.setEmail(rs.getString("email"));
+
+				// SCRIVO I DATI SU FILE
+				writer.write("Nome: " + ana.getNome() + ", ");
+	            writer.write("Cognome: " + ana.getCognome() + ", ");
+	            writer.write("ID: " + ana.getId() + ", ");
+	            writer.write("Telefono: " + ana.getTelefono() + ", ");
+	            writer.write("Indirizzo: " + ana.getIndirizzo() + ", ");
+	            writer.write("Codice fiscale: " + ana.getCodiceFiscale() + ", ");
+	            writer.write("Email: " + ana.getEmail() + "\n");
+
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				dbconn.close();
+				writer.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
 
 	}
 }
--- src/main/java/it/logicainformatica/progettoSpring/rest/AnagraficaController.java
+++ src/main/java/it/logicainformatica/progettoSpring/rest/AnagraficaController.java
@@ -1,8 +1,17 @@
 package it.logicainformatica.progettoSpring.rest;
 
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,6 +34,13 @@ public class AnagraficaController {
 	@ResponseStatus(HttpStatus.OK)
 	public void inserisciAnagrafica(@RequestBody AnagraficaUtente anag) {
 		a.inserisciUtente(anag);
+		
+		try {
+			a.scrivoSuFile(anag);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@GetMapping("/getListaAnagrafica")
@@ -32,4 +48,39 @@ public class AnagraficaController {
 		List<AnagraficaUtente> lista = a.getAnagraficaAll();
 		return lista;
 	}
+	
+	
+	
+	@GetMapping("/leggi-file")
+	public ResponseEntity<String> leggiDati() {
+		
+	    try {
+	    	
+	        // CREO UN OGGETTO BUFFEREDREDADER PER LEGGERE IL CONTENUTO DEL FILE
+	        BufferedReader reader = new BufferedReader(new FileReader("prova.txt"));
+
+	        // LEGGO IL CONTENUTO DEL FILE E LO METTO IN UNA STRINGA
+	        StringBuilder sb = new StringBuilder();
+	        String line;
+	        while ((line = reader.readLine()) != null) {
+	            sb.append(line);
+	            sb.append("\n");
+	        }
+
+	        // CHIUDO L'OGGETTO PER LEGGERE IL FILE
+	        reader.close();
+
+	        // RESTITUISCO IL CONTENUTO DEL FILE COME RESPONSE HTTP
+	        HttpHeaders headers = new HttpHeaders();
+	        headers.setContentType(MediaType.TEXT_PLAIN);
+	        return new ResponseEntity<String>(sb.toString(), headers, HttpStatus.OK);
+
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	        // IN CASO DI ERRORE RESTITUISCO UN MESSAGGIO DI ERRORE CON UNO STATO HTTP 500
+	        return new ResponseEntity<String>("Errore durante la lettura del file", HttpStatus.INTERNAL_SERVER_ERROR);
+	    }
+	}
+
+	
 }
